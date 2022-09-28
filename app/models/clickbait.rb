b073f8fbf17bd60780c5edffc8c1ed7d64bb43eb
Olga Nadongo <winnienadongo@gmail.com>
2022-09-28 16:43:25 +0300

    completed lab

--- app/models/clickbait.rb
+++
@@ -1,9 +0,0 @@
-class ClickbaitValidator<ActiveModel::EachValidator
-    # EachValidator is an ActiveRecordmodule
-    def validate_each(record,attribute,value)
-        #logic
-        unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
-            record.errors[attribute] << ("Not a clickbait title")
-          end
-    end
-end
